49b6526768b4ff6db76ea1ef775fa06df0d3fde2
Mohit Gupta <mohit.gupta@arcsinfotech.com>
2024-05-08 11:47:57 +0530

    okta auth flow

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,8 +6,21 @@ import { defineAuth } from "@aws-amplify/backend";
  */
 export const auth = defineAuth({
   loginWith: {
-    email: {
-      verificationEmailSubject: "Welcome! Verify your email!",
+    externalProviders: {
+      callbackUrls: ["https://main.d3exkpgsr1867b.amplifyapp.com/"],
+      logoutUrls: ["https://main.d3exkpgsr1867b.amplifyapp.com/"],
+      saml: {
+        metadata: {
+          metadataType: "URL",
+          metadataContent:
+            "https://dev-04704684.okta.com/app/exkgsxnb85xTvUkPR5d7/sso/saml/metadata",
+        },
+        attributeMapping: {
+          email:
+            "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
+        },
+        name: "oktaa-test",
+      },
     },
   },
 });
