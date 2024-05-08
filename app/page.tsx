de7ccf5c6bbb94d527c9e8b0c1d0c2c9be94aee1
Mohit Gupta <mohit.gupta@arcsinfotech.com>
2024-05-08 16:23:19 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,10 @@ import { useEffect } from "react";
 const App = () => {
   useEffect(() => {
     setTimeout(() => {
+      console.log("inside timeout");
       redirect("/dashboard");
     }, 0);
+    console.log("useEffect");
   });
   return <div className="App"></div>;
 };
